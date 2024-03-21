8f61039d15e170d59f36f10fa49b0d3e43f63266
KaminaPL <malytankus@gmail.com>
2024-03-21 14:12:08 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String s = "aa,bb,cc";
+        String[] splited = s.split(",");
+        for (String str:
+             splited) {
+            System.out.print(str);
+        }
+    }
+}
