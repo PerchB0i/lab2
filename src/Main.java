8f40f3ecf886f6df4b01c7dab43f19293fdec0aa
KaminaPL <malytankus@gmail.com>
2024-03-21 14:47:07 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,51 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        //stringi
+        /*
         String s = "aa,bb,cc";
         String[] splited = s.split(",");
-        for (String str:
-             splited) {
-            System.out.print(str);
+        for (String str : splited) {
+            System.out.print(str + ' ');
+        }
+
+         */
+//listy
+/*
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("test");
+        for (String str : list) {
+            System.out.print(str + ' ');
+        }
+        */
+//data
+        /*
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String dateString = "10.10.2010";
+        LocalDate date = LocalDate.parse(dateString, formatter);
+        */
+        //pliczki
+        try {
+            FileReader fileReader = new FileReader("C:\\Users\\student\\Desktop\\oop\\lab2\\family.csv");
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line = reader.readLine();
+            while(line != null) {
+                //...line
+                line = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
